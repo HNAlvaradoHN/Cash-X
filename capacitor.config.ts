639cfee7e8f8504fa073118c2763bf99f1e3ee54
HNAlvaradoHN <150305232+HNAlvaradoHN@users.noreply.github.com>
2026-09-16 11:44:01 -0600

    fix(android): remove incompatible TypeScript config

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.cashx.app',
-  appName: 'Cash-X',
-  webDir: 'dist',
-};
-
-export default config;
